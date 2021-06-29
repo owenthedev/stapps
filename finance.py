8f2c7d28a948f1b6d0ddac1fcc5411ad27b0b372
owenthedev <86202551+owenthedev@users.noreply.github.com>
2021-06-30 01:28:05 +0200

    Update finance.py

--- finance.py
+++ finance.py
@@ -17,11 +17,12 @@ st.image('https://images.unsplash.com/photo-1579621970795-87facc2f976d?ixid=Mnwx
 st.write('Not sure who it was, but someone said that compund interest is the 8th wonder of the world. This person said so because it has a powerful effect on growth as time goes on.')
 
 st.write('')
+st.write('#Sliding into your bank balance')
 int_slider=st.slider('Interest rate %, Slide to choose your interest rate', min_value=1, max_value=20)
 deposit=100
 n=2
 future_value=round((deposit*(1+(int_slider/100))**n),2)
 st.write(f'Assuming a ${deposit} deposit into a savings account over {n} years, your money would grow to the value of:')
-st.write(f'# R{future_value}')
+st.write(f'## R{future_value}')
 #fp="https://raw.githubusercontent.com/owenthedev/stapps/main/applestock.csv"         
 #df=pd.read_csv(filepath_or_buffer=fp)
