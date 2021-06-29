faa3149468f5e1b9f1de4e6c586264f64d44f6fa
owenthedev <86202551+owenthedev@users.noreply.github.com>
2021-06-30 01:40:49 +0200

    Update finance.py

--- finance.py
+++ finance.py
@@ -17,12 +17,26 @@ st.image('https://images.unsplash.com/photo-1579621970795-87facc2f976d?ixid=Mnwx
 st.write('Not sure who it was, but someone said that compund interest is the 8th wonder of the world. This person said so because it has a powerful effect on growth as time goes on.')
 
 st.write('')
-st.write('# Sliding into your bank balance')
+st.write("""# Sliding into your bank balance
+            ## Compund Interest-
+ """)
 int_slider=st.slider('Interest rate %, Slide to choose your interest rate', min_value=1, max_value=20)
 deposit=100
 n=2
 future_value=round((deposit*(1+(int_slider/100))**n),2)
 st.write(f'Assuming a R{deposit} deposit into a savings account over {n} years, your money would grow to the value of:')
 st.write(f'## R{future_value}')
+
+st.write('')
+st.write("""# Sliding into your bank balance
+            ## Simple Interest-
+ """)
+int_slider2=st.slider('Interest rate %, Slide to choose your interest rate', min_value=1, max_value=20)
+
+future_value2=round((deposit*(1+(int_slider2/100))*n),2)
+st.write(f'Assuming a R{deposit} deposit into a savings account over {n} years, your money would grow to the value of:')
+st.write(f'## R{future_value}')
+st.write('')
+st.write('Comparing the final result between the two types of interest we can see that, given the same initial deposit, time period and rate,compound interest is superior. Even though the standard is to offer compound interest rates, banks have tried to lure depositors by using a high simple interest rate to make it appear that their offer is better.')
 #fp="https://raw.githubusercontent.com/owenthedev/stapps/main/applestock.csv"         
 #df=pd.read_csv(filepath_or_buffer=fp)
