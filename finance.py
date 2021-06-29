b0c26d8b1249c667cabd72c79f822070523e1795
owenthedev <86202551+owenthedev@users.noreply.github.com>
2021-06-30 01:16:36 +0200

    Added slider

--- finance.py
+++ finance.py
@@ -14,5 +14,11 @@ st.write("""
          Owen Nxumalo
          """)
 st.image('https://images.unsplash.com/photo-1579621970795-87facc2f976d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fGZpbmFuY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60')
+st.write('Not sure who it was, but someone said that compund interest is the 8th wonder of the world. This person said so because it has a powerful effect on growth as time goes on.')
+int_slider=st.slider('Interest rate %', min_value=1, max_value=20)
+deposit=100
+n=2
+future_value=deposit*(1+(int_slider/100))^n
+st.write(f'Assuming a ${deposit} deposit into a savings account over {n} years, your money would grow to the value of R{future_value}')
 #fp="https://raw.githubusercontent.com/owenthedev/stapps/main/applestock.csv"         
 #df=pd.read_csv(filepath_or_buffer=fp)
