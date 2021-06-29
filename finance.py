62879ce29ad56504e22b5df12831b8aeb52f302e
owenthedev <86202551+owenthedev@users.noreply.github.com>
2021-06-30 01:05:21 +0200

    Update finance.py

--- finance.py
+++ finance.py
@@ -3,3 +3,16 @@
 Created on Sun Jun 20 18:32:32 2021
 @author: owenn
 """
+import numpy as np
+import pandas as pd
+import streamlit as st
+st.title("This week on Stocks")
+st.write("""
+         ## Interest(ing): Simple vs Compound
+         30 June, 2021
+         
+         Owen Nxumalo
+         """)
+st.image('https://images.unsplash.com/photo-1579621970795-87facc2f976d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fGZpbmFuY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60')
+#fp="https://raw.githubusercontent.com/owenthedev/stapps/main/applestock.csv"         
+#df=pd.read_csv(filepath_or_buffer=fp)
